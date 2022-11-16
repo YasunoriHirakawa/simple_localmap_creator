fa5718681e752500e32c72bc49be8cceda6ef883
ibuki1805 <ee173131@meiji.ac.jp>
2022-11-16 21:03:39 +0900

    Surport to asymmetry road width. Project walls on avoiding robot space when robot come back to road.

--- src/road_projector.cpp
+++ src/road_projector.cpp
@@ -1,10 +1,28 @@
 #include<road_projector/road_projector.h>
 
+State::State(float _robot_length)
+{
+    robot_length = _robot_length;
+    is_outside_of_road = false;
+    project_virtual_wall = false;
+    std::vector<float> pre_point = {0.0, 0.0};
+    std::vector<std::vector<float>> pre_virtual_wall_zone = { pre_point, pre_point, pre_point, pre_point};
+    virtual_wall_zone0 = pre_virtual_wall_zone;
+    virtual_wall_zone1 = pre_virtual_wall_zone;
+    geometry_msgs::Point pre_point_msg;
+    pre_point_msg.x = 0.0;
+    pre_point_msg.y = 0.0;
+    pre_point_msg.z = 0.0;
+    start_point = pre_point_msg;
+    end_point = pre_point_msg;
+}
+
 RoadProjector::RoadProjector(): nh_(""), tf_listener_(tf_buffer_)
 {
     sub_map_ = nh_.subscribe("/localmap", 1, &RoadProjector::map_callback, this, ros::TransportHints().tcpNoDelay());
     sub_road_ = nh_.subscribe("/road", 1, &RoadProjector::road_callback, this, ros::TransportHints().tcpNoDelay());
     pub_projected_map_ = nh_.advertise<nav_msgs::OccupancyGrid>("/projected_map", 1);
+    // tmr_listen_tf_ = nh_.createTimer(ros::Duration(0.05), &RoadProjector::listen_tf, this);
 
     gridmap_ = new Gridmap();
 
@@ -12,8 +30,11 @@ RoadProjector::RoadProjector(): nh_(""), tf_listener_(tf_buffer_)
     private_nh.param<bool>("visualize", visualize_, false);
     private_nh.param<float>("road_thickness", road_thickness_, {0.2});
     private_nh.param<float>("ignore_wall_radius", ignore_wall_radius_, {2.0});
+    float robot_length;
+    private_nh.param<float>("robot_length", robot_length, {0.5});
     have_received_road_ = false;
     is_outside_of_road_ = false;
+    current_state_ = new State(robot_length);
     if(visualize_)
     {
         pub_road_points_ = nh_.advertise<geometry_msgs::PoseArray>("/road_points", 1);
@@ -27,20 +48,44 @@ RoadProjector::~RoadProjector(){}
 void RoadProjector::map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
     if(!have_received_road_) return;
-    if(on_node() || out_of_road())
-    {
-        pub_projected_map_.publish(msg);
-        return;
-    }
     if(msg->data.size() == 0)
     {
         ROS_WARN("mapdata is empty");
         return;
     }
+    if(visualize_)
+    {
+        road_points_.poses.clear();
+        road_points_.header.frame_id = "base_link";
+        int i = 0;
+        for(const auto& point: current_state_->virtual_wall_zone0)
+        {
+            geometry_msgs::Pose pose;
+            pose.position.x = point[State::Point::X];
+            pose.position.y = point[State::Point::Y];
+            road_points_.poses.push_back(pose);
+            i++;
+        }
+        i = 0;
+        for(const auto& point: current_state_->virtual_wall_zone1)
+        {
+            geometry_msgs::Pose pose;
+            pose.position.x = point[State::Point::X];
+            pose.position.y = point[State::Point::Y];
+            road_points_.poses.push_back(pose);
+            i++;
+        }
+        pub_road_points_.publish(road_points_);
+    }
+    if(on_node() || !current_state_->project_virtual_wall)
+    {
+        pub_projected_map_.publish(msg);
+        return;
+    }
     if(gridmap_->set_map(*msg))
     {
         // project road zone to map
-        project_road_to_map(road_, gridmap_);
+        project_road_to_map(gridmap_);
         publish_projected_map();
     }
     else
@@ -52,31 +97,14 @@ void RoadProjector::map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 
 void RoadProjector::road_callback(const amsl_navigation_msgs::Road::ConstPtr& msg)
 {
-    road_ = set_road(*msg);
-    if (!road_.size()) return;
+    if(!set_road(*msg)) return;
     have_received_road_ = true;
-}
-
-bool RoadProjector::out_of_road()
-{
-    if(is_outside_of_road_)
-    {
-        //road_zone is inside of wall
-        std::vector<std::vector<float>> road_zone = { road_[0], road_[3], road_[7], road_[4] };
-        return !is_in_polygon(0.0, 0.0, road_zone);
-    }
-    else
-    {
-        //road_zone is outside of wall
-        std::vector<std::vector<float>> road_zone = { road_[1], road_[2], road_[6], road_[5] };
-        return !is_in_polygon(0.0, 0.0, road_zone);
-    }
 }
 
 bool RoadProjector::on_node()
 {
-    float node_x = 0.5*(road_[0][0] + road_[4][0]);
-    float node_y = 0.5*(road_[0][1] + road_[4][1]);
+    float node_x = current_state_->start_point.x;
+    float node_y = current_state_->start_point.y;
     if(visualize_)
     {
         geometry_msgs::PoseStamped node_point;
@@ -86,14 +114,14 @@ bool RoadProjector::on_node()
         node_point.pose.position.y = node_y;
         node_point.pose.orientation.w = 1.0;
         pub_node_point_.publish(node_point);
-        ROS_INFO("dist_from_node: %f", std::hypot(node_x, node_y));
+        // ROS_INFO("dist_from_node: %f", std::hypot(node_x, node_y));
     }
 
     if(std::hypot(node_x, node_y) < ignore_wall_radius_) return true;
     else return false;
 }
 
-void RoadProjector::project_road_to_map(const std::vector<std::vector<float>> &road, Gridmap *gridmap)
+void RoadProjector::project_road_to_map(Gridmap *gridmap)
 {
     double max_x = gridmap->get_width()*gridmap->get_resolution() + gridmap->get_origin_x();
     double min_x = gridmap->get_origin_x();
@@ -103,17 +131,14 @@ void RoadProjector::project_road_to_map(const std::vector<std::vector<float>> &r
     {
         for(double y = min_y; y < max_y; y += gridmap->get_resolution())
         {
-            if(is_in_virtual_wall_zone(x, y, road)) gridmap->set_grid(y, x, 100);
+            if(is_in_virtual_wall_zone(x, y)) gridmap->set_grid(y, x, 100);
         }
     }
 }
 
-bool RoadProjector::is_in_virtual_wall_zone(const double x, const double y, const std::vector<std::vector<float>> &road)
+bool RoadProjector::is_in_virtual_wall_zone(const double x, const double y)
 {
-    std::vector<std::vector<float>> polygon0 = {road[0], road[1], road[2], road[3]};
-    std::vector<std::vector<float>> polygon1 = {road[4], road[5], road[6], road[7]};
-
-    if(is_in_polygon(x, y, polygon0) || is_in_polygon(x, y, polygon1)) return true;
+    if(is_in_polygon(x, y, current_state_->virtual_wall_zone0) || is_in_polygon(x, y, current_state_->virtual_wall_zone1)) return true;
     else return false;
 }
 
@@ -136,9 +161,10 @@ bool RoadProjector::is_in_polygon(const double x, const double y, const std::vec
     else return true;
 }
 
-std::vector<std::vector<float>> RoadProjector::set_road(const amsl_navigation_msgs::Road &road)
+bool RoadProjector::set_road(const amsl_navigation_msgs::Road &road)
 {
-    std::vector<std::vector<float>> road_points;
+    current_state_->start_point = road.point0;
+    current_state_->end_point = road.point1;
     geometry_msgs::TransformStamped transform;
     try
     {
@@ -147,31 +173,169 @@ std::vector<std::vector<float>> RoadProjector::set_road(const amsl_navigation_ms
     catch(tf2::TransformException &ex)
     {
         ROS_WARN("%s", ex.what());
-        road_points = road_;
-        return road_points;
+        return false;
     }
-    float width_sin = 0.5*road.width*std::sin(road.direction);
-    float width_cos = 0.5*road.width*std::cos(road.direction);
-    float thickness_sin = (road_thickness_ + 0.5*road.width)*std::sin(road.direction);
-    float thickness_cos = (road_thickness_ + 0.5*road.width)*std::cos(road.direction);
-    // first line points
-    road_points.push_back({road.point0.x - width_sin, road.point0.y + width_cos});
-    road_points.push_back({road.point0.x - thickness_sin, road.point0.y + thickness_cos});
-    road_points.push_back({road.point1.x - thickness_sin, road.point1.y + thickness_cos});
-    road_points.push_back({road.point1.x - width_sin, road.point1.y + width_cos});
-    // second line points
-    road_points.push_back({road.point0.x + width_sin, road.point0.y - width_cos});
-    road_points.push_back({road.point0.x + thickness_sin, road.point0.y - thickness_cos});
-    road_points.push_back({road.point1.x + thickness_sin, road.point1.y - thickness_cos});
-    road_points.push_back({road.point1.x + width_sin, road.point1.y - width_cos});
-    if(visualize_)
+    tf2::doTransform(current_state_->start_point, current_state_->start_point, transform);
+    tf2::doTransform(current_state_->end_point, current_state_->end_point, transform);
+
+
+    //zone0 = right
+    float rightside_width = road.distance_to_right;
+    float leftside_width = road.width - road.distance_to_right;
+    std::vector<float> width_sin = {rightside_width*std::sin(road.direction), leftside_width*std::sin(road.direction)};
+    std::vector<float> width_cos = {rightside_width*std::cos(road.direction), leftside_width*std::cos(road.direction)};
+    std::vector<float> thickness_sin = {(road_thickness_ + rightside_width)*std::sin(road.direction), (road_thickness_ + leftside_width)*std::sin(road.direction)};
+    std::vector<float> thickness_cos = {(road_thickness_ + rightside_width)*std::cos(road.direction), (road_thickness_ + leftside_width)*std::cos(road.direction)};
+
+    if(current_state_->is_outside_of_road)
     {
-        road_points_.poses.clear();
-        road_points_.header.frame_id = "base_link";
+        std::vector<std::vector<float>> check_zone; //outside of road
+        check_zone.push_back({road.point0.x - width_sin[0], road.point0.y + width_cos[0]});
+        check_zone.push_back({road.point1.x - width_sin[0], road.point1.y + width_cos[0]});
+        check_zone.push_back({road.point1.x + width_sin[1], road.point1.y - width_cos[1]});
+        check_zone.push_back({road.point0.x + width_sin[1], road.point0.y - width_cos[1]});
+        for(auto& point: check_zone) point = transform_road(point, transform);
+        set_virtual_wall_points(road, transform, width_sin, width_cos, thickness_sin, thickness_cos);
+        if(is_in_polygon(0.0, 0.0, check_zone))
+        {
+            current_state_->project_virtual_wall = true;
+            current_state_->is_outside_of_road = adjust_virtual_wall_points(road);
+            ROS_ERROR_STREAM("outside: " << current_state_->is_outside_of_road);
+        }
+        else current_state_->project_virtual_wall = false;
+
     }
-    for(auto& road: road_points) road = transform_road(road, transform);
-    if(visualize_) pub_road_points_.publish(road_points_);
-    return road_points;
+    else
+    {
+        std::vector<std::vector<float>> check_zone; //outside of wall
+        check_zone.push_back({road.point0.x - thickness_sin[0], road.point0.y + thickness_cos[0]});
+        check_zone.push_back({road.point1.x - thickness_sin[0], road.point1.y + thickness_cos[0]});
+        check_zone.push_back({road.point1.x + thickness_sin[1], road.point1.y - thickness_cos[1]});
+        check_zone.push_back({road.point0.x + thickness_sin[1], road.point0.y - thickness_cos[1]});
+        for(auto& point: check_zone) point = transform_road(point, transform);
+        set_virtual_wall_points(road, transform, width_sin, width_cos, thickness_sin, thickness_cos);
+        current_state_->is_outside_of_road = !is_in_polygon(0.0, 0.0, check_zone);
+        if(current_state_->is_outside_of_road) current_state_->project_virtual_wall = false;
+        else current_state_->project_virtual_wall = true;
+    }
+
+    return true;
+}
+
+bool RoadProjector::adjust_virtual_wall_points(const amsl_navigation_msgs::Road& road)
+{
+    // if adjusted return true, not need to adjust return false
+    if(robot_is_side_of_zone0())
+    {
+        // ROS_INFO("robot is rightside of road");
+        int count = 0;
+        float resolution = 0.1;
+        int max_count = (int)((road_thickness_ + current_state_->robot_length)/resolution);
+        float dx10 = (current_state_->virtual_wall_zone0[1][State::Point::X] - current_state_->virtual_wall_zone0[0][State::Point::X]) / road_thickness_ * resolution;
+        float dy10 = (current_state_->virtual_wall_zone0[1][State::Point::Y] - current_state_->virtual_wall_zone0[0][State::Point::Y]) / road_thickness_ * resolution;
+        while(is_hitting_robot(current_state_->virtual_wall_zone0))
+        {
+            current_state_->virtual_wall_zone0[0][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone0[0][State::Point::Y] += dy10;
+            current_state_->virtual_wall_zone0[1][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone0[1][State::Point::Y] += dy10;
+
+            current_state_->virtual_wall_zone0[2][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone0[2][State::Point::Y] += dy10;
+            current_state_->virtual_wall_zone0[3][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone0[3][State::Point::Y] += dy10;
+
+            count++;
+            if(count > max_count)
+            {
+                ROS_WARN("road projector: adjested wall max times %d", count);
+                break;
+            }
+        }
+        if(count) return true;
+        else return false;
+    }
+    else
+    {
+        // ROS_INFO("robot is leftside of road");
+        int count = 0;
+        float resolution = 0.1;
+        int max_count = (int)((road_thickness_ + current_state_->robot_length)/resolution);
+        float dx10 = (current_state_->virtual_wall_zone1[1][State::Point::X] - current_state_->virtual_wall_zone1[0][State::Point::X]) / road_thickness_ * resolution;
+        float dy10 = (current_state_->virtual_wall_zone1[1][State::Point::Y] - current_state_->virtual_wall_zone1[0][State::Point::Y]) / road_thickness_ * resolution;
+        // ROS_INFO_STREAM("dx10: " << dx10 << " dy10: " << dy10);
+        // float dx23 = (current_state_->virtual_wall_zone1[3][State::Point::X] - current_state_->virtual_wall_zone1[2][State::Point::X]) / road_thickness_ * resolution;
+        // float dy23 = current_state_->virtual_wall_zone1[3][State::Point::Y] - current_state_->virtual_wall_zone1[2][State::Point::Y] / road_thickness_ * resolution;
+        while(is_hitting_robot(current_state_->virtual_wall_zone1) )
+        {
+            current_state_->virtual_wall_zone1[0][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone1[0][State::Point::Y] += dy10;
+            current_state_->virtual_wall_zone1[1][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone1[1][State::Point::Y] += dy10;
+
+            current_state_->virtual_wall_zone1[2][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone1[2][State::Point::Y] += dy10;
+            current_state_->virtual_wall_zone1[3][State::Point::X] += dx10;
+            current_state_->virtual_wall_zone1[3][State::Point::Y] += dy10;
+
+            count++;
+            if(count > max_count)
+            {
+                ROS_WARN("road projector: adjested wall max times %d", count);
+                break;
+            }
+        }
+        // ROS_INFO_STREAM("count: " << count);
+        if(count) return true;
+        else return false;
+    }
+}
+
+bool RoadProjector::is_hitting_robot(const std::vector<std::vector<float>>& zone)
+{
+    float wall_distance = calc_distance_between_point_and_line(zone[0], zone[3], {0.0, 0.0});
+    // ROS_WARN_STREAM("wall_distance: " << wall_distance << " robot_length: " << current_state_->robot_length);
+    if(wall_distance < current_state_->robot_length) return true;
+    else return false;
+}
+
+float RoadProjector::calc_distance_between_point_and_line(const std::vector<float>& l1, const std::vector<float>& l2, const std::vector<float>& point)
+{
+    float dx21 = l2[0] - l1[0];
+    float dy21 = l2[1] - l1[1];
+    float dx10 = l1[0] - point[0];
+    float dy10 = l1[1] - point[1];
+    float dx20 = l2[0] - point[0];
+    float dy20 = l2[1] - point[1];
+
+    double t = -(dx21*dx10 + dy21*dy10);
+    if(t < 0) return std::hypot(dx10, dy10);
+    else if(t > dx21*dx21 + dy21*dy21) return std::hypot(dx20, dy20);
+    else return sqrt((dx21*dy10 - dy21*dx10) * (dx21*dy10 - dy21*dx10) / (dx21*dx21 + dy21*dy21));
+}
+
+bool RoadProjector::robot_is_side_of_zone0()
+{
+    float d0 = std::hypot(current_state_->virtual_wall_zone0[0][State::Point::X], current_state_->virtual_wall_zone0[0][State::Point::Y]);
+    float d1 = std::hypot(current_state_->virtual_wall_zone1[0][State::Point::X], current_state_->virtual_wall_zone1[0][State::Point::Y]);
+    if(d0 < d1) return true;
+    else return false;
+}
+
+void RoadProjector::set_virtual_wall_points(const amsl_navigation_msgs::Road& road, const geometry_msgs::TransformStamped& transform, const std::vector<float>& width_sin, const std::vector<float>& width_cos, const std::vector<float>& thickness_sin, const std::vector<float>& thickness_cos)
+{
+        // first line points
+        current_state_->virtual_wall_zone0[0] = {road.point0.x - width_sin[0], road.point0.y + width_cos[0]};
+        current_state_->virtual_wall_zone0[1] = {road.point0.x - thickness_sin[0], road.point0.y + thickness_cos[0]};
+        current_state_->virtual_wall_zone0[2] = {road.point1.x - thickness_sin[0], road.point1.y + thickness_cos[0]};
+        current_state_->virtual_wall_zone0[3] = {road.point1.x - width_sin[0], road.point1.y + width_cos[0]};
+        // second line points
+        current_state_->virtual_wall_zone1[0] = {road.point0.x + width_sin[1], road.point0.y - width_cos[1]};
+        current_state_->virtual_wall_zone1[1] = {road.point0.x + thickness_sin[1], road.point0.y - thickness_cos[1]};
+        current_state_->virtual_wall_zone1[2] = {road.point1.x + thickness_sin[1], road.point1.y - thickness_cos[1]};
+        current_state_->virtual_wall_zone1[3] = {road.point1.x + width_sin[1], road.point1.y - width_cos[1]};
+        for(auto& point: current_state_->virtual_wall_zone0) point = transform_road(point, transform);
+        for(auto& point: current_state_->virtual_wall_zone1) point = transform_road(point, transform);
 }
 
 std::vector<float> RoadProjector::transform_road(const std::vector<float> &road, const geometry_msgs::TransformStamped &transform)
@@ -182,7 +346,6 @@ std::vector<float> RoadProjector::transform_road(const std::vector<float> &road,
     pose.position.z = 0;
     pose.orientation.w = 1.0;
     tf2::doTransform(pose, pose, transform);
-    if(visualize_) road_points_.poses.push_back(pose);
     return {pose.position.x, pose.position.y};
 }
 
